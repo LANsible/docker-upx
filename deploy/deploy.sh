d688a332d5bf220b07bd2438e74da3eecdab1373
wilmardo <info@wilmardenouden.nl>
2020-08-04 10:19:57 +0200

    feat: switch to Gitlab for builds
    Signed-off-by: wilmardo <info@wilmardenouden.nl>

--- deploy/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Deploying ${DOCKER_NAMESPACE}/${CONTAINER_NAME}:${ARCH}"
-echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
-docker push ${DOCKER_NAMESPACE}/${CONTAINER_NAME}
